ecab896c716129dcffb24735f7f6a0f1f9cf9ee4
lanfuli <lanfuli007@gmail.com>
2016-11-24 05:28:59 -0800

    Add Manipulator ALGORITHM and Selection
    This is not complete, not test them yet.

--- RitchieLanMP5/public_html/src/MainController.js
+++ RitchieLanMP5/public_html/src/MainController.js
@@ -19,7 +19,7 @@ myModule.controller("MainCtrl", function ($scope) {
     // Initialize the graphics system
     gEngine.Core.initializeWebGL('GLCanvas');
     $scope.mCanvasMouse = new CanvasMouseSupport('GLCanvas');
-    
+
     // Radio button selection support
     $scope.eSelection = [
         {label: "Parent"},
@@ -28,24 +28,24 @@ myModule.controller("MainCtrl", function ($scope) {
 //        {label: "RightChild"},
     ];
 
-       // this is the model
+    // this is the model
     $scope.mMyWorld = new ClassExample();
     $scope.mSelectedXform = $scope.mMyWorld.parentXform();
     $scope.mSelectedEcho = $scope.eSelection[0].label;
-    
+
     $scope.mMouseOver = "Nothing";
     $scope.mLastWCPosX = 0;
     $scope.mLastWCPosY = 0;
 
     $scope.mView = new Camera(
-                [0, 3],         // wc Center
-                15,                // wc Wdith
-                [0, 0, 800, 600]);  // viewport: left, bottom, width, height
+            [0, 3], // wc Center
+            15, // wc Wdith
+            [0, 0, 800, 600]);  // viewport: left, bottom, width, height
 
     $scope.mainTimerHandler = function () {
         // 1. update the world
         $scope.mMyWorld.update();
-        
+
         // Step E: Clear the canvas
         gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1]);        // Clear the canvas
         //
@@ -54,30 +54,54 @@ myModule.controller("MainCtrl", function ($scope) {
     };
 
     $scope.serviceSelection = function (event) {
-        
-        var eventPixelPos = [0,0];
+
+        var eventPixelPos = [0, 0];
         eventPixelPos[0] = $scope.mCanvasMouse.getPixelXPos(event);
         eventPixelPos[1] = $scope.mCanvasMouse.getPixelYPos(event);
-        
+
         $scope.mMyWorld.checkClick($scope.pixelToWc(eventPixelPos));
     };
     $scope.pixelToWc = function (pixelPos)
     {
         //note this is hard coded for this window and coordinate system
-        var wcPosition = [0,0];
+        var wcPosition = [0, 0];
         var ratio = 800 / 15;
         wcPosition[0] = (pixelPos[0] - 400) / ratio;
         wcPosition[1] = ((pixelPos[1] - 300) / ratio) + 3;
-        
+
         return wcPosition;
     };
-    
+
     $scope.serviceMove = function (event) {
-        var pixelPos = [0,0];
+        var pixelPos = [0, 0];
         pixelPos[0] = $scope.mCanvasMouse.getPixelXPos(event);
         pixelPos[1] = $scope.mCanvasMouse.getPixelYPos(event);
         $scope.mLastWCPosX = this.mView.mouseWCX(pixelPos[0]);
         $scope.mLastWCPosY = this.mView.mouseWCY(pixelPos[1]);
-        $scope.mMyWorld.setPositionOfSelected($scope.pixelToWc(pixelPos))
+        $scope.mMyWorld.setPositionOfSelected($scope.pixelToWc(pixelPos));
+        $scope.mMouseOver = $scope.mMyWorld.detectMouseOver($scope.mLastWCPosX, $scope.mLastWCPosY, (event.which === 1));
+    };
+
+    $scope.isMouseOnScaleKnob = false;
+    $scope.isMouseOnRotationKnob = false;
+    $scope.isMouseOnTranslationKnob = false;
+
+    $scope.knobPos = function (event) {
+        var scaleKnobIndex = $scope.mMouseOver.indexOf("Manipulator Rotation Knob");
+        var rotationKnobIndex = $scope.mMouseOver.indexOf("Manipulator Rotation Knob");
+
+        if (scaleKnobIndex !== -1) {
+            $scope.isMouseOnScaleKnob = true;
+            $scope.isMouseOnRotationKnob = false;
+            $scope.isMouseOnTranslationKnob = false;
+        } else if (rotationKnobIndex !== -1) {
+            $scope.isMouseOnScaleKnob = false;
+            $scope.isMouseOnTranslationKnob = false;
+            $scope.isMouseOnRotationKnob = true;
+        } else {
+            $scope.isMouseOnScaleKnob = false;
+            $scope.isMouseOnRotationKnob = false;
+            $scope.isMouseOnTranslationKnob = true;
+        }
     };
 });
